9fce83d5a3f8f9d9e853d55d227330863e342520
ELNURA <e84485517@gmail.com>
2026-03-05 15:10:49 +0600

    class.py

---
+++ class.py
@@ -0,0 +1,13 @@
+class Car:
+   def __init__(self,color, wheel, type, prise):
+     self.color = color
+     self.wheel = wheel
+     self.type = type
+     self.prise = prise 
+
+p1 = Car("red", 4, "m5f90", 100000 )
+
+print(p1.color)
+print(p1.wheel)
+print(p1.type)
+print(p1.prise)
